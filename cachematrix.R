00fc7bed51759e17759c42a97c5fddb1e5ca645a
philipm <31691069+philipmattocks@users.noreply.github.com>
2018-07-12 11:24:04 +0100

    Coursera R programming assignment 2 submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions find the inverse of a matrix,
+## using a cached value if availible
 
-## Write a short comment describing this function
+## makeCacheMatrix creates an list of functions 
+## and returns these to the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## cachesolve takes an object of type makeCacheMatrix
+## and calculates the matrix inverse if it doesn't already
+## exist as a cached object.  If it exists it returns the
+## cached value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
